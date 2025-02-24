36071df523aae2258818df9753eb4eddeee1b64b
KyPaToP <burtik.yakov@gmail.com>
2025-02-24 18:26:14 +0300

    ver 0.0.2

--- RProPlugin/MainWindow.xaml.cs
+++ RProPlugin/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,16 +11,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RProPlugin.Models;
 
 namespace RProPlugin;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 public partial class MainWindow : Window
 {
+    List<Component> _jsonData = new List<Component>();
+    ObservableCollection<Component> Components { get; set; }
+    
+    
     public MainWindow()
     {
         InitializeComponent();
+        Components = new ObservableCollection<Component>
+        {
+            new Component { Name = "Объект1", Category = "Категория1" },
+            new Component { Name = "Объект2", Category = "Категория2" },
+            new Component { Name = "Объект3", Category = "Категория3"}
+        };
+        MyDataGrid.ItemsSource = Components;
+        Console.WriteLine(Components[0].Name);
+        DataContext = this;
     }
+
+    private void ReadJsonOnClick(object sender, RoutedEventArgs e)
+    {
+        string json = File.ReadAllText("../../../specification.json");
+        _jsonData = JsonSerializer.Deserialize<List<Component>>(json);
+        if (_jsonData.Count == 0)
+        {
+            Console.WriteLine("No specification found");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Ok");
+            Components.Clear();
+            foreach (var component in _jsonData)
+            {
+                Components.Add(component);
+            }
+        }
+    }
+    
 }
---
+++ RProPlugin/Models/Component.cs
@@ -0,0 +1,8 @@
+﻿namespace RProPlugin.Models;
+
+public class Component
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+
+}
